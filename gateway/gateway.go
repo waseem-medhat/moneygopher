d1a0acb481104aef04a61a63aeee45a5bd30f426
wipdev-tech <wipdev.tech@gmail.com>
2024-04-08 20:57:29 +0200

    Implement dummy gateway client to gRPC service

---
+++ gateway/gateway.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/wipdev-tech/microbank/transactions"
+	"google.golang.org/genproto/googleapis/type/money"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	opts := []grpc.DialOption{}
+	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	conn, err := grpc.Dial("localhost:8080", opts...)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	trxClient := transactions.NewTransactionsClient(conn)
+
+	ctx := context.Background()
+	resp, err := trxClient.Deposit(
+		ctx,
+		&transactions.DepositRequest{
+			AccountID: "",
+			Amount:    &money.Money{CurrencyCode: "USD", Units: 1000},
+			Otp:       1234,
+		},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(resp.NewBalance)
+}
