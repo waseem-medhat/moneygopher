d39ff919d24706ae34113b980ad46405c0b8f2d6
wipdev-tech <wipdev.tech@gmail.com>
2024-04-18 19:05:26 +0200

    Add directory creation to codegen

--- codegen/main.go
+++ codegen/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	if len(os.Args) != 2 {
+		fmt.Println("must provide 1 arg")
+		os.Exit(1)
+	}
+	serviceName := os.Args[1]
+
+	_, err := os.Stat(serviceName)
+	if !os.IsNotExist(err) {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	if err := os.Mkdir(fmt.Sprintf("services/%s", serviceName), 0755); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	if err := os.Mkdir(fmt.Sprintf("services/%s/cmd", serviceName), 0755); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	if err := os.Mkdir(fmt.Sprintf("services/%s/bin", serviceName), 0755); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
