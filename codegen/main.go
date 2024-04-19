a37514e7974f0b8ffe7cb07fc26cde15e27e6c3d
wipdev-tech <wipdev.tech@gmail.com>
2024-04-19 12:07:07 +0200

    Generate Dockerfile from template

--- codegen/main.go
+++ codegen/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"text/template"
 )
 
 func main() {
@@ -18,18 +19,45 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err := os.Mkdir(fmt.Sprintf("services/%s", serviceName), 0755); err != nil {
+	err = makeDirs(serviceName)
+	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
+	}
+
+	err = makeDockerfile(serviceName)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func makeDirs(serviceName string) error {
+	if err := os.Mkdir(fmt.Sprintf("services/%s", serviceName), 0755); err != nil {
+		return err
 	}
 
 	if err := os.Mkdir(fmt.Sprintf("services/%s/cmd", serviceName), 0755); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		return err
 	}
 
 	if err := os.Mkdir(fmt.Sprintf("services/%s/bin", serviceName), 0755); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		return err
 	}
+
+	return nil
+}
+
+func makeDockerfile(serviceName string) error {
+	t, err := template.ParseFiles("codegen/Dockerfile.template")
+	if err != nil {
+		return err
+	}
+
+	file, err := os.Create(fmt.Sprintf("services/%s/Dockerfile", serviceName))
+	if err != nil {
+		return err
+	}
+
+	return t.Execute(file, serviceName)
 }
