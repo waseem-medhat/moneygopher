71699e1e9b65f095d912a2fdb7c509634d571413
wipdev-tech <wipdev.tech@gmail.com>
2024-04-21 14:40:00 +0200

    Create OTP server with gRPC/ProtoBuf setup

--- scripts/proto_gen.sh
+++ scripts/proto_gen.sh
@@ -4,4 +4,5 @@ protoc --go_out=. --go_opt=paths=source_relative \
     --go-grpc_out=. --go-grpc_opt=paths=source_relative \
     proto/money.proto \
     services/transactions/transactions.proto \
-    services/accounts/accounts.proto
+    services/accounts/accounts.proto \
+    services/otp/otp.proto
